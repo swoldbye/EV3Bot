2cd4561262c4f3901422a89ab8c084c4e4a6be9b
Simon Woldbye-Lyng <s180943@student.dtu.dk>
2020-03-04 12:13:17 +0100

    Testing a commit for Mark.

--- src/server/ConnectionSettings.java
+++ src/server/ConnectionSettings.java
@@ -3,5 +3,5 @@ package server;
 public class ConnectionSettings {
 
     public static final int PORT = 4444;
-    public static final String IP = "172.20.10.8";
+    public static final String IP = "172.20.10.7";
 }
